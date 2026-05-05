afaa8eb5bc32585d0628dbf510db0194dfd12d8b
niranjan-023 <niranjanrn50@gmail.com>
2026-05-05 10:12:55 +0530

    Tailwind setup

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,9 @@
 function App() {
   return (
-    <div>
-      <h1>LeafNote 🌿</h1>
+    <div className="min-h-screen flex items-center justify-center bg-green-900">
+      <h1 className="text-4xl font-bold text-blue-600">
+        Tailwind Working 🚀
+      </h1>
     </div>
   );
 }
