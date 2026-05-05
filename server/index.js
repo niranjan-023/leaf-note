6bd86a4354cfd1b3add9185520a6f473c55f07bf
niranjan-023 <niranjanrn50@gmail.com>
2026-05-05 18:09:47 +0530

    Connect MongoDB Atlas database

--- server/index.js
+++ server/index.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
@@ -9,6 +10,11 @@ const app = express();
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+// DB Connection
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => console.log("MongoDB Connected ✅"))
+  .catch((err) => console.log(err));
 
 // Test route
 app.get("/", (req, res) => {
